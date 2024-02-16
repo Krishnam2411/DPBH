e96ccdbe1add02416ad27c86a334bacd6266ebbd
krishnam <maheshwarikrishnam123@gmail.com>
2024-02-16 18:42:33 +0530

    FIX: Tabs background switching

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,62 +1,100 @@
+const storage = chrome.storage.session
+  ? chrome.storage.session
+  : chrome.storage.local;
+
+async function getActivation(tabId) {
+  return Object.values(await storage.get(`_id_${tabId}`))[0];
+}
+
+async function setActivation(tabId, activation) {
+  return await storage.set({ [`_id_${tabId}`]: activation });
+}
+
+async function removeActivation(tabId) {
+  return await storage.remove(`_id_${tabId}`);
+}
+
+async function getResults(tabId) {
+  return Object.values(await storage.get(`_results_${tabId}`))[0];
+}
+
+async function getActivationOrSetDefault(tabId) {
+  let activation = await getActivation(tabId);
+  if (activation === undefined) {
+    activation = true;
+    await setActivation(tabId, activation);
+  }
+  return activation;
+}
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "setBadge") {
-    chrome.action.setBadgeBackgroundColor({ color: "yellow" });
-    chrome.action.setBadgeText({ text: `${message.count}` });
+  if ("results" in message) {
+    getActivation(sender.tab.id).then((activation) => {
+      if (activation === true) {
+        saveResults(message.results, sender.tab.id);
+        displayBadge(message.results.Count, sender.tab.id);
+      }
+      sendResponse({ success: true });
+    });
+  } else if ("enableExtension" in message && "tabId" in message) {
+    setActivation(message.tabId, message.enableExtension).then(() => {
+      if (message.enableExtension === false) {
+        displayBadge("", message.tabId);
+        chrome.tabs.sendMessage(
+          message.tabId,
+          { action: "remove" },
+          function (response) {
+            console.log(response);
+          }
+        );
+      } else
+        chrome.tabs.sendMessage(
+          message.tabId,
+          { action: "highlightPatterns" },
+          function (response) {
+            console.log(response);
+          }
+        );
+      console.log(message);
+      sendResponse({ success: true });
+    });
+  } else if ("action" in message && message.action == "getActivationState") {
+    let tabId;
+    if ("tabId" in message) {
+      tabId = message.tabId;
+    } else {
+      tabId = sender.tab.id;
+    }
+    getActivationOrSetDefault(tabId).then((activation) => {
+      sendResponse({ isEnabled: activation });
+    });
+  } else {
+    sendResponse({ success: false });
   }
-  if (message.action === "removeBadge") {
-    chrome.action.setBadgeText({ text: "" });
-  }
+  return true;
 });
 
-chrome.tabs.onActivated.addListener(function (activeInfo) {
-  chrome.storage.local.get(["switchState"], (res) => {
-    if (Boolean(res.switchState)) {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (tabs && tabs[0]) {
-          const activeTab = tabs[0];
-          chrome.tabs.reload(activeTab.id);
-          chrome.action.setBadgeText({ text: "" });
-          console.log("Active tab ID:", activeTab.id);
-          chrome.webNavigation.onCompleted.addListener((details) => {
-            if (details.frameId === 0 && details.tabId === activeInfo.tabId) {
-              console.log("DOM loaded. Sending 'detect' message.");
-              chrome.tabs.sendMessage(
-                activeTab.id,
-                { action: "detect" },
-                function (response) {
-                  if (chrome.runtime.lastError) {
-                    console.error(chrome.runtime.lastError.message);
-                  } else {
-                    console.log("Message response:", response);
-                  }
-                }
-              );
-            }
-          });
-        } else {
-          console.error("No active tab found.");
-        }
-      });
-    } else {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (tabs && tabs[0]) {
-          const activeTab = tabs[0];
-          console.log("Active tab ID:", activeTab.id);
-          chrome.tabs.sendMessage(
-            activeTab.id,
-            { action: "remove" },
-            function (response) {
-              if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError.message);
-              } else {
-                console.log("Message response:", response);
-              }
-            }
-          );
-        } else {
-          console.error("No active tab found.");
-        }
-      });
-    }
-  });
+chrome.tabs.onReplaced.addListener(async function (addedTabId, removedTabId) {
+  await setActivation(addedTabId, await getActivation(removedTabId));
+  await removeActivation(removedTabId);
 });
+
+chrome.tabs.onRemoved.addListener(async function (tabId, removeInfo) {
+  await removeActivation(tabId);
+});
+
+async function saveResults(results, tabId) {
+  return await storage.set({ [`_results_${tabId}`]: results });
+}
+
+function displayBadge(count, tabId) {
+  chrome.action.setBadgeText({
+    tabId: tabId,
+    text: "" + count,
+  });
+  let bgColor = "yellow";
+  chrome.action.setBadgeBackgroundColor({
+    tabId: tabId,
+    color: bgColor,
+  });
+}
