944693363bb0e91109b1147a232e44916d35ac76
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2024-02-27 22:54:23 +0530

    deployed database to neon

--- api/database/database.py
+++ api/database/database.py
@@ -1,11 +1,19 @@
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+# Load environment variables from the .env file
+load_dotenv()
+# Load the model path from the environment variable
+key=os.getenv("db_key")
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+SQLALCHEMY_DATABASE_URL = f'{key}'
 
 engine = _sql.create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
